5e3e3b566e6259cb57c487e802827af3b3037f3a
Lorezz <point.point@gmail.com>
2019-05-07 17:07:37 +0200

    update to expo 30 and fixed graphql calls

--- components/TalkFooter.js
+++ components/TalkFooter.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { format } from 'date-fns';
-import { Colors, Layout } from '../constants';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { format } from "date-fns";
+import { Colors, Layout } from "../constants";
 
 export default class TalkFooter extends React.PureComponent {
   render() {
     const { details } = this.props;
-    const formattedStart = format(details.eventStart, 'h:mmA');
-
+    const formattedStart = format(details.eventStart, "h:mmA");
+    let room_name = details.room ? details.room.name : null;
+    if (room_name && room_name.length > 20) {
+      room_name = room_name.substring(0, 17) + `...`;
+    }
     return (
       <View style={styles.container}>
         <View style={styles.details}>
           <View style={styles.detail}>
             <Text style={styles.detailLabel}>Start</Text>
-            <Text style={styles.detailText}>
-              {formattedStart}
-            </Text>
+            <Text style={styles.detailText}>{formattedStart}</Text>
           </View>
           <View style={styles.detail}>
             <Text style={styles.detailLabel}>Duration</Text>
@@ -23,52 +24,58 @@ export default class TalkFooter extends React.PureComponent {
               {`${details.duration} Minutes`}
             </Text>
           </View>
+          {room_name && (
+            <View style={styles.detail}>
+              <Text style={styles.detailLabel}>Room</Text>
+              <Text
+                style={[styles.detailText, { color: details.room.color.hex }]}
+              >
+                {`${room_name}`}
+              </Text>
+            </View>
+          )}
         </View>
-
-
       </View>
     );
   }
-
-
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 2,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     paddingVertical: 13,
     paddingHorizontal: Layout.doubleBaseMargin,
     borderBottomLeftRadius: Layout.cardRadius,
     borderBottomRightRadius: Layout.cardRadius,
-    backgroundColor: Colors.silver,
+    backgroundColor: Colors.silver
   },
   details: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: "row"
   },
   detail: {
-    paddingRight: Layout.doubleBaseMargin,
+    paddingRight: Layout.doubleBaseMargin
   },
   detailLabel: {
-    fontFamily: 'Montserrat-Light',
+    fontFamily: "Montserrat-Light",
     fontSize: 11,
     color: Colors.lightText,
-    letterSpacing: 0,
+    letterSpacing: 0
   },
   detailText: {
-    fontFamily: 'Montserrat-SemiBold',
+    fontFamily: "Montserrat-SemiBold",
     fontSize: 11,
     color: Colors.darkPurple,
-    letterSpacing: 0,
+    letterSpacing: 0
   },
   remindMe: {
     flex: 1,
-    alignItems: 'stretch',
+    alignItems: "stretch"
   },
   socialButtons: {
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
+    alignItems: "center",
+    flexDirection: "row"
+  }
 });
--- screens/TalkDetailScreen.js
+++ screens/TalkDetailScreen.js
@@ -1,33 +1,32 @@
-import React from 'react';
+import React from "react";
 import {
   Animated,
   StyleSheet,
   Platform,
   Image,
   Text,
-  View,
-} from 'react-native';
-import FadeIn from 'react-native-fade-in-image';
+  View
+} from "react-native";
+import FadeIn from "react-native-fade-in-image";
 
-import BackButton from '../components/BackButton';
-import SocialMediaButton from '../components/SocialMediaButton';
-import TalkFooter from '../components/TalkFooter';
-import StatusBarUnderlay from '../components/StatusBarUnderlay';
-import Colors from '../constants/Colors';
-import Layout from '../constants/Layout';
+import BackButton from "../components/BackButton";
+import SocialMediaButton from "../components/SocialMediaButton";
+import TalkFooter from "../components/TalkFooter";
+import StatusBarUnderlay from "../components/StatusBarUnderlay";
+import Colors from "../constants/Colors";
+import Layout from "../constants/Layout";
 
 export default class TalkDetailScreen extends React.PureComponent {
   state = {
-    scrollY: new Animated.Value(0),
+    scrollY: new Animated.Value(0)
   };
 
   render() {
     const { details } = this.props.navigation.state.params;
-
     let underlayOpacity = this.state.scrollY.interpolate({
       inputRange: [0, 50],
       outputRange: [0, 1],
-      extrapolate: 'clamp',
+      extrapolate: "clamp"
     });
 
     return (
@@ -37,7 +36,8 @@ export default class TalkDetailScreen extends React.PureComponent {
           onScroll={Animated.event(
             [{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }],
             { useNativeDriver: true }
-          )}>
+          )}
+        >
           <View style={styles.container}>
             <BackButton style={styles.backButton} />
 
@@ -46,12 +46,8 @@ export default class TalkDetailScreen extends React.PureComponent {
             {this._renderAvatar()}
             <View style={styles.card}>
               <Text style={styles.sectionHeading}>TALK</Text>
-              <Text style={styles.heading}>
-                {details.title}
-              </Text>
-              <Text style={styles.description}>
-                {details.description}
-              </Text>
+              <Text style={styles.heading}>{details.title}</Text>
+              <Text style={styles.description}>{details.description}</Text>
               <Text style={styles.sectionHeading}>ABOUT</Text>
               {this._renderSpeakers()}
             </View>
@@ -67,16 +63,14 @@ export default class TalkDetailScreen extends React.PureComponent {
 
   _renderAvatar() {
     const { details } = this.props.navigation.state.params;
-
     const image = (
-      <Image style={styles.avatar} source={{ uri: details.avatarURL }} />
+      <Image
+        style={styles.avatar}
+        source={{ uri: details.speakerInfo[0].image.url }}
+      />
     );
-    if (Platform.OS === 'ios') {
-      return (
-        <FadeIn style={styles.avatarFadeContainer}>
-          {image}
-        </FadeIn>
-      );
+    if (Platform.OS === "ios") {
+      return <FadeIn style={styles.avatarFadeContainer}>{image}</FadeIn>;
     } else {
       return image;
     }
@@ -92,25 +86,23 @@ export default class TalkDetailScreen extends React.PureComponent {
   _renderSpeaker = (speaker, index) => {
     return (
       <View key={index}>
-        <Text style={styles.heading}>
-          {speaker.name}
-        </Text>
-        <Text style={styles.description}>
-          {speaker.bio}
-        </Text>
+        <Text style={styles.heading}>{speaker.name}</Text>
+        <Text style={styles.description}>{speaker.bio}</Text>
         <View style={styles.social}>
-          {speaker.twitter &&
+          {speaker.twitter && (
             <SocialMediaButton
               network="twitter"
               spacing="right"
               username={speaker.twitter}
-            />}
-          {speaker.github &&
+            />
+          )}
+          {speaker.github && (
             <SocialMediaButton
               network="github"
               spacing="right"
               username={speaker.github}
-            />}
+            />
+          )}
         </View>
       </View>
     );
@@ -125,14 +117,14 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 101,
     marginBottom: Layout.doubleBaseMargin,
-    marginHorizontal: Layout.doubleBaseMargin,
+    marginHorizontal: Layout.doubleBaseMargin
   },
   backButton: {
-    position: 'absolute',
+    position: "absolute",
     top: -65,
     left: -5,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center"
   },
   cardShadow1: {
     flex: 1,
@@ -140,7 +132,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     backgroundColor: Colors.purpleShadow1,
     borderTopLeftRadius: Layout.cardRadius,
-    borderTopRightRadius: Layout.cardRadius,
+    borderTopRightRadius: Layout.cardRadius
   },
   cardShadow2: {
     flex: 1,
@@ -148,20 +140,20 @@ const styles = StyleSheet.create({
     marginHorizontal: 5,
     backgroundColor: Colors.purpleShadow2,
     borderTopLeftRadius: Layout.cardRadius,
-    borderTopRightRadius: Layout.cardRadius,
+    borderTopRightRadius: Layout.cardRadius
   },
   card: {
     paddingTop: 48,
     paddingHorizontal: 30,
     borderTopLeftRadius: Layout.cardRadius,
     borderTopRightRadius: Layout.cardRadius,
-    backgroundColor: Colors.snow,
+    backgroundColor: Colors.snow
   },
   avatarFadeContainer: {
-    zIndex: 4,
+    zIndex: 4
   },
   avatar: {
-    position: 'absolute',
+    position: "absolute",
     zIndex: 4,
     top: -43,
     left: (Layout.screenWidth - Layout.doubleBaseMargin * 2) / 2 - 53,
@@ -169,33 +161,33 @@ const styles = StyleSheet.create({
     width: 106,
     borderRadius: 53,
     borderColor: Colors.snow,
-    borderWidth: 1,
+    borderWidth: 1
   },
   sectionHeading: {
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
     marginBottom: 12,
-    fontFamily: 'Montserrat-Light',
+    fontFamily: "Montserrat-Light",
     fontSize: 11,
     letterSpacing: 3,
-    color: Colors.lightText,
+    color: Colors.lightText
   },
   heading: {
     marginBottom: 5,
-    fontFamily: 'Montserrat-SemiBold',
+    fontFamily: "Montserrat-SemiBold",
     fontSize: 17,
     letterSpacing: 0,
-    color: Colors.darkPurple,
+    color: Colors.darkPurple
   },
   description: {
     marginBottom: 30,
-    fontFamily: 'Montserrat-Light',
+    fontFamily: "Montserrat-Light",
     fontSize: 16,
     letterSpacing: 0,
     lineHeight: 24,
-    color: Colors.lightText,
+    color: Colors.lightText
   },
   social: {
-    flexDirection: 'row',
-    marginBottom: 30,
-  },
+    flexDirection: "row",
+    marginBottom: 30
+  }
 });
