fc6871082a775a70d196417a9e42a0dec16b889c
Irene Oppo <i.oppo@datocms.com>
2020-04-15 12:53:50 +0200

    upgraded expo and react-native dep

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+module.exports = (() => {
+  return {
+    resolver: {
+      assetExts: ['txt', 'xml', 'png', 'jpg', 'pb', 'tflite', 'ttf'],
+      sourceExts: ['txt', 'xml', 'png', 'jpg', 'pb', 'tflite'],
+    },
+  };
+})();
